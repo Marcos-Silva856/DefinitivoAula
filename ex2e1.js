27d14ba37853ca1dcc3ea6a06612cd6ce1c38085
MARCOS VINICIUS DA SILVA <21887@unifacef.edu.br>
2019-08-28 20:35:36 -0300

    28 do 8

--- ex2e1.js
+++ ex2e1.js
@@ -1,4 +1,4 @@
-function ex1(){
+function ex2(){
     var salario = [];
     var qtdfilho = [];
     var idade = [];
@@ -37,7 +37,7 @@ function ex1(){
     alert('Maior salario: '+Objeto.salario);
     alert('porcentagem de mulheres > 1000 salario: '+ Objeto.sexo);
 }
-function  ex2(){
+function  ex3(){
     var idade = [];
     var sexo = [];
     var altura = [];
@@ -66,3 +66,70 @@ function  ex2(){
     }
 
 }
+function ex4(){
+    var vet = [];
+    var Media = 0;
+    var menorIdade = 9999;
+    var maiorIdade = 9999;
+    var mulher = 0;
+    for(var i = 0; i < 3; i++){
+        var objeto = {};
+        objeto.idade = Number(prompt("Digite sua idade:"));
+        objeto.sexo = prompt("Digite seu sexo");
+        objeto.renda = Number(prompt("Digite sua renda"));
+        objeto.filhos = Number(prompt("Digite quantos filhos você tem"));
+        Media += objeto.renda;
+        if(maiorIdade < objeto.idade){
+            maiorIdade = objeto.idade
+        }
+        if(menorIdade > objeto.idade){
+            menorIdade = objeto.idade
+        }
+        if(objeto.filhos > 2 && objeto.renda < 600 && objeto.sexo == 'F'){
+            mulher++;
+        }
+        vet.push(objeto);
+    }
+    Media = Media / i;
+    alert("Media de salarios: "+ Media);
+    alert("Menor e maior idade: "+ menorIdade +" "+ maiorIdade);
+    alert("Mulher mais de 2 filhos e inferior a 600 "+mulher);
+}
+
+function ex5(){
+    var vet = []
+    for(var i=0; i<5;i++){
+        var objeto = {}
+        objeto.codigo = Number(prompt("Codigo"));
+        objeto.descricao = prompt("Descricao");
+        objeto.qtde = Number(prompt("Qtde"));
+        objeto.preco = Number(prompt("Idade"))
+        for(var j = 0; j<vet.length; j++){
+            if(objeto.codigo < vet[j].codigo){
+                vet.slice(j, 0, objeto);
+                alert("Adicionado");
+                break;
+            }
+        }
+        if(j == vet.length){
+            vet.push(objeto);
+            alert("Adicionado 1 ou ult");
+        }
+    }
+    for(var i = 0; i < vet.length; i++){
+        alert(vet[i].codigo + ' '+ vet[i].descricao)
+    }
+
+    var cod =Number(promt("Informe codigo"))
+    for(var i = 0; i<5; i++){
+        if(cod==vet[i].codigo){
+            vet[i].descricao = prompt("Descriação")
+            vet[i].descricao = prompt("Qtde")
+            vet[i].descricao = prompt("reco")
+            break
+        }
+    }
+    if(i==5){
+        alert("Produto não existe")
+    }
+ }
